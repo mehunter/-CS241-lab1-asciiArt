357d5875c9fcbf8a2245d9ffd2ef8169106931d4
Matt Hunter <thematthunter@gmail.com>
2016-02-25 09:21:05 -0700

    completed lab assignment

---
+++ asciiArt.s
@@ -0,0 +1,40 @@
+	.file	"asciiArt.c"
+	.section	.rodata
+	.align 4
+.LC0:
+	.ascii	"\n /$$      /$$             /$$     /$$    \n| $$$    /$$$  "
+	.ascii	"          | $$    | $"
+	.string	"$    \n| $$$$  /$$$$  /$$$$$$  /$$$$$$ /$$$$$$  \n| $$ $$/$$ $$ |____  $$|_  $$_/|_  $$_/  \n| $$  $$$| $$  /$$$$$$$  | $$    | $$    \n| $$\\  $ | $$ /$$__  $$  | $$ /$$| $$ /$$\n| $$ \\/  | $$|  $$$$$$$  |  $$$$/|  $$$$/\n|__/     |__/ \\_______/   \\___/   \\___/\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	leal	4(%esp), %ecx
+	.cfi_def_cfa 1, 0
+	andl	$-16, %esp
+	pushl	-4(%ecx)
+	pushl	%ebp
+	.cfi_escape 0x10,0x5,0x2,0x75,0
+	movl	%esp, %ebp
+	pushl	%ecx
+	.cfi_escape 0xf,0x3,0x75,0x7c,0x6
+	subl	$4, %esp
+	subl	$12, %esp
+	pushl	$.LC0
+	call	puts
+	addl	$16, %esp
+	movl	$0, %eax
+	movl	-4(%ebp), %ecx
+	.cfi_def_cfa 1, 0
+	leave
+	.cfi_restore 5
+	leal	-4(%ecx), %esp
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 5.2.1-22ubuntu2) 5.2.1 20151010"
+	.section	.note.GNU-stack,"",@progbits
